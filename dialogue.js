a9f5ef82b899558a015e6b9c97b748ade4befcc0
mwyatt <martin.wyatt@gmail.com>
2016-01-31 17:11:46 +0000

    dialogue basics tested and ready for simple use

--- dialogue.js
+++ dialogue.js
@@ -4,34 +4,6 @@ var mustache = require('mustache');
 var $document = $(document);
 var $window = $(window);
 
-var defaultOptions = {
-	templateContainer: '', // the mustache template container html
-	className: '', // to identify the dialogue uniquely
-
-	// optional
-	title: '',
-	description: '',
-	positionTo: '', // $selector where the dialogue will appear
-	hardClose: false, // make it difficult to close the dialogue
-	mask: false, // mask the page below
-	width: false, // int
-	html: '', // raw html to be placed in to body area, under description
-	ajaxConfig: false, // ajax - type, url, dataType, data, success, error
-	actions: {
-	  // {name: 'Cancel', action: function() {
-	  //   console.log('Cancel');
-	  // }},
-	  // {name: 'Ok', action: function() {
-	  //   console.log('Ok');
-	  // }}
-	},
-	onComplete: function() {}, // fired when dialogue has been rendered
-	onClose: function() {}, // fired when dialogue has been closed
-
-	// proposed
-	cssAnimation: false // to tell close whether to check for animation end? still a problem with browser compatibility
-};
-
 var templateContainer = '<div class="dialogue-container js-dialogue-container {{className}}">{{#mask}}	<div class="dialogue-mask js-dialogue-mask {{#className}}{{className}}-mask{{/className}}"></div>{{/mask}}<div class="dialogue js-dialogue {{#className}}{{className}}-dialogue{{/className}}"><span class="dialogue-close js-dialogue-close">&times;</span>{{#title}}<h6 class="dialogue-title">{{title}}</h6>{{/title}}{{#description}}<p class="dialogue-description">{{description}}</p>{{/description}}<div class="dialogue-html js-dialogue-html">{{{html}}}</div><div class="dialogue-actions">{{#actionNames}}<span class="button-primary dialogue-action js-dialogue-action" data-name="{{.}}">{{.}}</span>{{/actionNames}}</div></div></div>';
 
 var keyCode = {
@@ -68,18 +40,7 @@ function getRandomString () {
 
 // unchanging events
 // does event pass?
-var Dialogue = function (event) {
-
-	// click body means dont close
-	$document.on('click.dialogue.body', gS(classNames.dialogue), this, function(event) {
-		event.stopPropagation();
-	});
-
-	// clicking close [x]
-	$document.on('click.dialogue.close', gS(classNames.dialogueClose), this, function(event) {
-		event.data.close(event);
-	});
-};
+var Dialogue = function (event) {};
 
 
 // override if you wish to use your own dialogue template
@@ -92,6 +53,33 @@ Dialogue.prototype.setTemplateContainer = function(html) {
  * render, bind events, and position new dialogue
  */
 Dialogue.prototype.create = function(options) {
+	var defaultOptions = {
+		templateContainer: '', // the mustache template container html
+		className: '', // to identify the dialogue uniquely
+
+		// optional
+		title: '',
+		description: '',
+		positionTo: '', // $selector where the dialogue will appear
+		hardClose: false, // make it difficult to close the dialogue
+		mask: false, // mask the page below
+		width: false, // int
+		html: '', // raw html to be placed in to body area, under description
+		ajaxConfig: false, // ajax - type, url, dataType, data, success, error
+		actions: {
+		  // 'Cancel': function() {
+		  //   this.close();
+		  // },
+		  // 'Ok': function() {
+		  //   console.log('Ok');
+		  // }
+		},
+		onComplete: function() {}, // fired when dialogue has been rendered
+		onClose: function() {}, // fired when dialogue has been closed
+
+		// proposed
+		cssAnimation: false // to tell close whether to check for animation end? still a problem with browser compatibility
+	};
 	this.options = $.extend(defaultOptions, options);
 	this.options.className = this.options.className ? this.options.className : getRandomString();
 
@@ -116,77 +104,92 @@ Dialogue.prototype.create = function(options) {
 	this.applyCss(event);
 	this.setEvents(event);
 
-	if (this.options.ajaxConfig) {
-		var config = this.options.ajaxConfig;
+	// if (this.options.ajaxConfig) {
+	// 	var config = this.options.ajaxConfig;
+	// 	var isImage;
 
-		// var spin = new spinner(this.$dialogueHtml);
+	// 	// image or data?
+	// 	if (config.url.indexOf('.jpg') || config.url.indexOf('.gif') || config.url.indexOf('.png')) {
+	// 		isImage = true;
+	// 	};
 
-		$.ajax({
-			type: config.type,
-			url: config.url,
-			dataType: config.dataType,
-			data: config.data,
-			complete: function() {
-				event.data.options.onComplete.call();
-				event.data = applyCss();
-			},
-			success: function(response) {
-				config.success(response);
-			},
-			error: function(response) {
-				config.error(response);
-			}
-		});
+	// 	// var spin = new spinner(this.$dialogueHtml);
 
-	// no ajax
-	} else {
+	// 	$.ajax({
+	// 		type: config.type,
+	// 		url: config.url,
+	// 		dataType: config.dataType,
+	// 		data: config.data,
+	// 		complete: function() {
+	// 			event.data.options.onComplete.call();
+	// 			event.data.applyCss(event);
+	// 		},
+	// 		success: function(response) {
+	// 			config.success(response);
+	// 		},
+	// 		error: function(response) {
+	// 			config.error(response);
+	// 		}
+	// 	});
+
+	// // no ajax
+	// } else {
 
 		// completed build
 		this.options.onComplete.call();
-	};
+	// };
 };
 
 
 Dialogue.prototype.setEvents = function(event) {
 
-	// hard to close
-	if (event.data.options.hardClose) {
-		$document.off('keyup.dialogue.close');
-		$document.off('mousedown.dialogue.close');
-
-	// easy to close
-	} else {
+	// not hard to close
+	if (!event.data.options.hardClose) {
 
 		// hit esc
 		$document.on('keyup.dialogue.close', event.data, function(event) {
+			console.log('keyup.dialogue.close');
 			if (event.which == keyCode.esc) {
-				event.data.close(event);
+				event.data.closeWithEvent(event);
 			} 
 		});
 
 		// mousedown outside of dialogue
 		// down used because when clicking and dragging an input value will
 		// close it
-		$document.on('mousedown.dialogue.close', event.data, function(event) {
+		event.data.$container.on('mousedown.dialogue.close', event.data, function(event) {
+			console.log('mousedown.dialogue.close');
 			if (!$(event.target).closest(gS(classNames.dialogue)).length) {
-				event.data.close(event);
+				event.data.closeWithEvent(event);
 			}
 		});
 	};
 
 	// option actions [ok, cancel]
 	var actions = event.data.options.actions;
-	$document.off('click.dialogue.action');
 	if (actions) {
 		for (var actionName in actions) {
 			event.data.setActionEvent(event, actionName, actions[actionName]);
 		};
 	};
+
+	// click body means dont close
+	event.data.$container.on('click.dialogue.body', gS(classNames.dialogue), this, function(event) {
+		console.log('click.dialogue.body');
+		event.stopPropagation();
+	});
+
+	// clicking close [x]
+	event.data.$container.on('click.dialogue.close', gS(classNames.dialogueClose), this, function(event) {
+		console.log('click.dialogue.close');
+		event.data.closeWithEvent(event);
+	});
 };
 
 
 Dialogue.prototype.setActionEvent = function(event, actionName, actionFunction) {
-	$document.on('click.dialogue.action', '.js-dialogue-action[data-name="' + actionName + '"]', event.data, function(event) {
+	event.data.$container.on('click.dialogue.action', '.js-dialogue-action[data-name="' + actionName + '"]', event.data, function(event) {
+		console.log('click.dialogue.action');
 		actionFunction.call(event.data);
 	});
 };
@@ -258,14 +261,18 @@ Dialogue.prototype.closeWithEvent = function(event) {
 	// this.$container.addClass(removeClassName);
 	// this.$dialogue.on(getMotionEventName('animation'), function() {
 
+	// remove events
+	$document
+		// .off('click.dialogue.action')
+		.off('keyup.dialogue.close');
+	event.data.$container.off(); // needed?
 	event.data.$container.remove();
 	event.data.options.onClose.call();
 };
 
 
 Dialogue.prototype.close = function() {
-	this.$container.remove();
-	this.options.onClose.call();
+	this.closeWithEvent({data: this});
 };
 
 
