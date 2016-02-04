ad2a2a9b36547a15fc37476820c3c60ef2166db1
Martin Wyatt <martin.wyatt@gmail.com>
2016-02-04 17:03:50 +0000

    sublime project and trying to get dependencies correct

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
-var rename = require('gulp-rename');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-var eventStream = require('event-stream');
-var glob = require('glob');
 var autoprefixer = require('autoprefixer');
 var colorFunction = require('postcss-color-function');
 var hexrgba = require('postcss-hexrgba');
@@ -18,32 +15,15 @@ var processes = [
 ];
 
 gulp.task('css', function () {
-  return gulp.src('**.bundle.css')
+  return gulp.src('common.css')
     .pipe(postcss(processes))
-    .pipe(rename(function (pathObject) {
-      pathObject.basename = pathObject.basename.replace('.bundle', '');
-    }))
     .pipe(gulp.dest('asset'));
 });
 
 gulp.task('js', function(done) {
-  glob('**.bundle.js', function(err, files) {
-    if (err) {
-      done(err);
-    };
-    var tasks = files.map(function(entry) {
-      return browserify({
-        entries: [entry],
-        paths: [
-          '.'
-        ]
-      })
-      .bundle()
-      .pipe(source(entry.replace('js/', '').replace('.bundle', '')))
-      .pipe(gulp.dest('asset'));
-    });
-
-    // create a merged stream
-    eventStream.merge(tasks).on('end', done);
-  });
+  return browserify()
+    .add('common.js')
+    .bundle()
+    .pipe(source('common.js'))
+    .pipe(gulp.dest('asset'));
 });
