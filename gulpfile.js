758a969e52beaedcc866c6a81ce5932b84d309c6
Martin Wyatt <martin.wyatt@gmail.com>
2016-02-05 10:44:43 +0000

    deps correct and build OK

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,36 @@
+require('es6-promise').polyfill(); // could be required to fix postcss-import?
 var gulp = require('gulp');
+var gulpSrc = gulp.src;
+var plumber = require('gulp-plumber');
+var gutil = require('gulp-util');
 var postcss = require('gulp-postcss');
+var postcssImport = require('postcss-import');
+var postcssSimpleVars = require('postcss-simple-vars');
+var postcssMixins = require('postcss-mixins');
+var postcssColorFunction = require('postcss-color-function');
+var postcssHexrgba = require('postcss-hexrgba');
+var autoprefixer = require('autoprefixer');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-var autoprefixer = require('autoprefixer');
-var colorFunction = require('postcss-color-function');
-var hexrgba = require('postcss-hexrgba');
 var processes = [
-  require('postcss-import'),
-  require('postcss-mixins'),
-  require('postcss-simple-vars'),
-  hexrgba(),
-  colorFunction(),
+  postcssImport,
+  postcssMixins,
+  postcssSimpleVars,
+  postcssHexrgba(),
+  postcssColorFunction(),
   autoprefixer({browsers: ['last 1 version']})
 ];
+ 
+gulp.src = function() {
+  return gulpSrc.apply(gulp, arguments)
+    .pipe(plumber(function(error) {
+      // Output an error message
+      gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+      // emit the end event, to properly end the task
+      this.emit('end');
+    })
+  );
+};
 
 gulp.task('css', function () {
   return gulp.src('common.css')
@@ -21,7 +39,7 @@ gulp.task('css', function () {
 });
 
 gulp.task('js', function(done) {
-  return browserify()
+  return browserify({paths: ['.', 'node_modules']})
     .add('common.js')
     .bundle()
     .pipe(source('common.js'))
